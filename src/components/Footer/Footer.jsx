b584d893a47c5cd6e72106a4142c837ae0277734
Rashid Makki <rashidmakkiha_cse18@its.edu.in>
2020-05-18 07:21:36 +0530

    few changes made

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url || 'https://github.com/rashidmakki'}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
